7971de867ccc5ed7e41a110b091dea7feb416558
obed-tc <castroobed53@gmail.com>
2024-03-02 16:55:45 -0400

    Add server vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon({
-    iconDir: "src/icons",
-  })]
+  integrations: [
+    tailwind(),
+    icon({
+      iconDir: "src/icons",
+    }),
+  ],
+  output: "server",
+  adapter: vercel(),
 });
